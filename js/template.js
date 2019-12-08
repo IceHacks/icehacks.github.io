755bc4388c3329a7c97642dff7c10df34fae9dfd
IceHacks <38994479+IceHacks@users.noreply.github.com>
2019-12-07 14:39:52 -1000

    blog

--- js/template.js
+++ js/template.js
@@ -16,13 +16,22 @@ var versions = JSON.parse(
 	get("https://api.github.com/repos/IceHacks/SurvivCheatInjector/releases")
 );
 var latest = versions[0]["tag_name"];
+var blog = location.pathname.match(/blog/g);
+var blogdata;
+
+if (blog) {
+	blogData = JSON.parse(get("/blog/index.json"));
+}
+
+Vue.config.devtools = true;
 
 Vue.component("ice-header", {
-	props: ["name"],
+	props: ["name", "sub"],
 	template: `
 		<div class="header">
 			<div class="inner">
 				<span>{{name}}</span>
+				<span class="sub" v-if="sub">&nbsp{{sub}}</span>
 			</div>
 		</div>
 	`
@@ -45,10 +54,10 @@ Vue.component("ice-section", {
 	`
 });
 Vue.component("ice-box", {
-	props: ["color", "title", "url"],
+	props: ["color", "title", "url", "no"],
 	template: `
 		<div class="box" :style="(color ? 'background:'+color+';' : '') + (color ? isLight(color) ? 'color:black;' : 'color:white;' : '')">
-			<h1 v-if=title><span>{{title}}</span><a v-if=url v-bind:href=url target="_blank"><i class="material-icons">open_in_new</i></a></h1>
+			<h1 :style="(no ? 'margin:0;' : '')" v-if=title><span>{{title}}</span><a v-if=url v-bind:href=url target="_blank"><i class="material-icons">open_in_new</i></a></h1>
 			<slot></slot>
 		</div>
 	`,
@@ -66,7 +75,7 @@ Vue.component("ice-footer", {
 });
 Vue.component("ice-install", {
 	template: `
-		<ice-box color="#7c7d89" style="background: url(images/background.png);background-attachment: fixed;background-size: cover;max-height: 200px;overflow-y: hidden;box-shadow: inset 0 -55px 20px -30px #f1f1f1;border-radius: 6px 6px 0 0;">
+		<ice-box color="#7c7d89" style="background: url(/images/background.png);background-attachment: fixed;background-size: cover;max-height: 200px;overflow-y: hidden;box-shadow: inset 0 -55px 20px -30px #f1f1f1;border-radius: 6px 6px 0 0;">
 			<div class="install-box">
 				<div class="install-section">
 					<h1 style="margin:unset;">${latest}</h1>
@@ -111,6 +120,34 @@ Vue.component("ice-version", {
 });
 Vue.component("ice-downloads", {
 	template: `<span>${versions[0].assets[0]["download_count"]}</span>`
+});
+Vue.component("ice-sidebar", {
+	template: `
+		<ice-section size="0.8" width="450">
+			<ice-install></ice-install>
+			<ice-social color="#da3939" title="YouTube" button="SUBSCRIBE" button-color="#f1f1f1" url="https://youtube.com/c/IceHacks?sub_confirmation=1"></ice-social>
+			<ice-social color="#36393f" title="Discord" button="JOIN" button-color="#738bd7" url="https://discord.gg/dk6b5aj"></ice-social>
+		</ice-section>
+	`
+});
+Vue.component("ice-blog", {
+	template: `
+		<div>
+			<ice-box v-if="blog.length < 1">
+				Nothing here...
+			</ice-box>
+			<ice-box v-for="(b, id) in blog" :title=b.title>
+				{{b.content}}
+
+				<small class="date">{{new Date(b.date).toLocaleString()}}</small>
+			</ice-box>
+		</div>
+	`,
+	data: () => {
+		return {
+			blog: blogData || null
+		};
+	}
 });
 
 var app = new Vue({
