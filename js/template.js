5a3d1faf29593051cbc7dfdb2cdfaea33694790c
IceHacks <38994479+IceHacks@users.noreply.github.com>
2019-12-09 15:28:16 -1000

    Update template.js

--- js/template.js
+++ js/template.js
@@ -124,7 +124,7 @@ Vue.component("ice-sidebar", {
 		<ice-section size="0.8" width="450">
 			<ice-install></ice-install>
 			<ice-social color="#da3939" title="YouTube" button="SUBSCRIBE" button-color="#f1f1f1" url="https://youtube.com/c/IceHacks?sub_confirmation=1"></ice-social>
-			<ice-social color="#36393f" title="Discord" button="JOIN" button-color="#738bd7" url="https://discord.gg/dk6b5aj"></ice-social>
+			<ice-social color="#36393f" title="Discord" button="JOIN" button-color="#738bd7" url="https://discord.gg/j3S5vZM"></ice-social>
 		</ice-section>
 	`
 });
