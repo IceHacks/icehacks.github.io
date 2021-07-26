7be451c92948514cd9598b118674a127810a0151
IceHacks <38994479+IceHacks@users.noreply.github.com>
2021-07-26 11:41:15 -0400

    Update template.js

--- js/template.js
+++ js/template.js
@@ -113,7 +113,7 @@ Vue.component("ice-footer", {
 });
 Vue.component("ice-install", {
 	template: `
-		<ice-box color="#7c7d89" style="background: url(/images/background.png);background-attachment: fixed;background-size: cover;max-height: 200px;overflow-y: hidden;box-shadow: inset 0 -55px 20px -30px #f1f1f1;border-radius: 6px 6px 0 0;">
+		<ice-box color="#7c7d89" style="background: url("/images/background.png") 0% 0% / cover fixed;color: white;overflow-y: hidden;border-radius: 6px;">
 			<div class="install-box">
 				<div class="install-section">
 					<h1 style="margin:unset;">${latest}</h1>
